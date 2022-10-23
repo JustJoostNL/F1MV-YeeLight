feebc7c33ad043dd4f249d81927c6b1d24ba8395
koningcool <koningcool@outlook.com>
2022-10-23 17:50:37 +0200

    fix for econrefused error

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
     //begin of config file
 
-    // put the url of the livetiming api here
-    TrackStatusURL: 'http://localhost:10101/api/v1/live-timing/TrackStatus',
+    // put the url of the livetiming api here (USE THE IP OF YOUR SYSTEM, NOT LOCALHOST)
+    TrackStatusURL: 'http://:10101/api/v1/live-timing/TrackStatus',
     YeeLights: {
         // put all the ips of your lights here (use quotes)
         // if you have multiple lights seperated by a comma (Eg: "192.168.1.1", "192.168.1.2")
