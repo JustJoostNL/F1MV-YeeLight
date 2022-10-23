3e4a21beb875e35f3db01e3e08cb775d17c6cc37
koningcool <koningcool@outlook.com>
2022-10-23 17:59:15 +0200

    add electron files

--- main.js
+++ main.js
@@ -66,3 +66,35 @@ async function controlLights(brightness, color) {
 
 getTimingData();
 setInterval(getTimingData, 100);
+
+
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
