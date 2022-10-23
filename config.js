5bb5eb9fe1c3d630e31f0805f0a8da2527e3fa79
koningcool <koningcool@outlook.com>
2022-10-23 18:03:51 +0200

    update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     //begin of config file
 
     // put the url of the livetiming api here (USE THE IP OF YOUR SYSTEM, NOT LOCALHOST)
-    TrackStatusURL: 'http://:10101/api/v1/live-timing/TrackStatus',
+    TrackStatusURL: 'http://youriphere:10101/api/v1/live-timing/TrackStatus',
     YeeLights: {
         // put all the ips of your lights here (use quotes)
         // if you have multiple lights seperated by a comma (Eg: "192.168.1.1", "192.168.1.2")
