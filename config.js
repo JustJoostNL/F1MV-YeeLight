a8c7fe5245ddc505a3819a90802c696da5f857be
koningcool <koningcool@outlook.com>
2022-10-24 09:08:27 +0200

    * Add support for custom brightness * Add support for custom colors * Add support for custom blinks

--- config.js
+++ config.js
@@ -6,10 +6,56 @@ const config = {
     YeeLights: {
         // put all the ips of your lights here (use quotes)
         // if you have multiple lights seperated by a comma (Eg: "192.168.1.1", "192.168.1.2")
-        lights: ["ip1", "ip2"]
+        lights: ["ip1", "ip2"],
+
+    Settings: {
+        // put the brightness of your lights here (0-100)
+        brightness: 100,
+        blinkWhenGreenFlag: false,
+        blinkWhenRedFlag: true,
+        blinkWhenYellowFlag: false,
+        blinkWhenSafetyCar: false,
+        blinkWhenVSC: false,
+        blinkWhenVSCEnding: false,
+        timeBetweenBlinks: 500, // in ms
+
+        // put the color of your lights here (0-255)
+        green: {
+            r: 0,
+            g: 255,
+            b: 0
+        },
+        yellow: {
+            r: 255,
+            g: 255,
+            b: 0
+        },
+        red: {
+            r: 255,
+            g: 0,
+            b: 0
+        },
+        safetyCar: {
+            r: 255,
+            g: 255,
+            b: 0
+        },
+        vsc: {
+            r: 255,
+            g: 255,
+            b: 0
+        },
+        vscEnding: {
+            r: 255,
+            g: 255,
+            b: 0
+
+        }
+    }
+}
 
         //end of config file
-    }
+
 };
 
 module.exports = config;
--- main.js
+++ main.js
@@ -4,44 +4,120 @@ const config = require('./config');
 const { Bulb } = require('yeelight.io');
 const fetch = require('node-fetch').default;
 const url = config.TrackStatusURL;
-let check;
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+const brightnessSetting = config.YeeLights.Settings.brightness;
+
+const timeBetweenBlinks = config.YeeLights.Settings.timeBetweenBlinks;
+
+const greenColor = config.YeeLights.Settings.green;
+const yellowColor = config.YeeLights.Settings.yellow;
+const redColor = config.YeeLights.Settings.red;
+const safetyCarColor = config.YeeLights.Settings.safetyCar;
+const vscColor = config.YeeLights.Settings.vsc;
+const vscEndingColor = config.YeeLights.Settings.vscEnding;
+const blinkWhenGreenFlag = config.YeeLights.Settings.blinkWhenGreenFlag;
+const blinkWhenRedFlag = config.YeeLights.Settings.blinkWhenRedFlag;
+const blinkWhenYellowFlag = config.YeeLights.Settings.blinkWhenYellowFlag;
+const blinkWhenSafetyCar = config.YeeLights.Settings.blinkWhenSafetyCar;
+const blinkWhenVSC = config.YeeLights.Settings.blinkWhenVSC;
+const blinkWhenVSCEnding = config.YeeLights.Settings.blinkWhenVSCEnding;
 
 const allLights = config.YeeLights.lights;
 
+
+let check;
+
+
 async function getTimingData(){
-    let a = await fetch(url);
+    let a = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
     const data = await a.json();
 
     if(check !== data.Status) {
         switch (data.Status) {
             case "1":
                 console.log("Green")
-                await controlLights(100, 0,255, 0);
+                await controlLightsOn(brightnessSetting, greenColor.r, greenColor.g, greenColor.b);
+                if(blinkWhenGreenFlag) {
+                    // blink the lights 5 times async
+                    for (let i = 0; i < 5; i++) {
+                        await controlLightsOff();
+                        await sleep(timeBetweenBlinks);
+                        await controlLightsOn(brightnessSetting, greenColor.r, greenColor.g, greenColor.b);
+                        await sleep(timeBetweenBlinks);
+                    }
+                }
                 check = data.Status;
                 break;
             case "2":
                 console.log("Yellow")
-                await controlLights(100, 255,255, 0);
+                await controlLightsOn(brightnessSetting, yellowColor.r, yellowColor.g, yellowColor.b);
+                if(blinkWhenYellowFlag) {
+                    for (let i = 0; i < 5; i++) {
+                        await controlLightsOff();
+                        await sleep(timeBetweenBlinks);
+                        await controlLightsOn(brightnessSetting, yellowColor.r, yellowColor.g, yellowColor.b);
+                        await sleep(timeBetweenBlinks);
+                    }
+                }
                 check = data.Status;
                 break;
             case "4":
                 console.log("SC")
-                await controlLights(100, 255, 255, 0);
+                await controlLightsOn(brightnessSetting, safetyCarColor.r, safetyCarColor.g, safetyCarColor.b);
+                if(blinkWhenSafetyCar) {
+                    for (let i = 0; i < 5; i++) {
+                        await controlLightsOff();
+                        await sleep(timeBetweenBlinks);
+                        await controlLightsOn(brightnessSetting, safetyCarColor.r, safetyCarColor.g, safetyCarColor.b);
+                        await sleep(timeBetweenBlinks);
+                    }
+                }
                 check = data.Status;
                 break;
             case "5":
                 console.log("Red")
-                await controlLights(100, 255, 0, 0);
+                await controlLightsOn(brightnessSetting, redColor.r, redColor.g, redColor.b);
+                if(blinkWhenRedFlag) {
+                    for (let i = 0; i < 5; i++) {
+                        await controlLightsOff();
+                        await sleep(timeBetweenBlinks);
+                        await controlLightsOn(brightnessSetting, redColor.r, redColor.g, redColor.b);
+                        await sleep(timeBetweenBlinks);
+                    }
+                }
                 check = data.Status;
                 break;
             case "6":
                 console.log("VCS")
-                await controlLights(100, 255, 255, 0);
+                await controlLightsOn(brightnessSetting, vscColor.r, vscColor.g, vscColor.b);
+                if(blinkWhenVSC) {
+                    for (let i = 0; i < 5; i++) {
+                        await controlLightsOff();
+                        await sleep(timeBetweenBlinks);
+                        await controlLightsOn(brightnessSetting, vscColor.r, vscColor.g, vscColor.b);
+                        await sleep(timeBetweenBlinks);
+                    }
+                }
                 check = data.Status;
                 break;
             case "7":
                 console.log("VSC Ending")
-                await controlLights(100, 255, 255, 0);
+                await controlLightsOn(brightnessSetting, vscEndingColor.r, vscEndingColor.g, vscEndingColor.b);
+                if(blinkWhenVSCEnding) {
+                    for (let i = 0; i < 5; i++) {
+                        await controlLightsOff();
+                        await sleep(timeBetweenBlinks);
+                        await controlLightsOn(brightnessSetting, vscEndingColor.r, vscEndingColor.g, vscEndingColor.b);
+                        await sleep(timeBetweenBlinks);
+                    }
+                }
                 check = data.Status;
                 break;
         }
@@ -49,7 +125,7 @@ async function getTimingData(){
 
 }
 
-async function controlLights(brightness, r, g, b) {
+async function controlLightsOn(brightness, r, g, b) {
     const brightnessValue = brightness;
 
     allLights.forEach((light) => {
@@ -59,6 +135,23 @@ async function controlLights(brightness, r, g, b) {
                 lamp.color(r,g,b);
                 lamp.brightness(brightnessValue);
                 lamp.onn();
+                lamp.disconnect();
+            } catch (err) {
+                console.log(err)
+            }
+        });
+        bulb.connect();
+    });
+}
+
+
+async function controlLightsOff() {
+
+    allLights.forEach((light) => {
+        const bulb = new Bulb(light);
+        bulb.on('connected', (lamp) => {
+            try {
+                lamp.off();
                 lamp.disconnect();
             } catch (err) {
                 console.log(err)
